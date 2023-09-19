746b8618b2e96b9568c3b18f4136ad06bc15919a
YaroslavDen1sov <undertaker.61370@.com>
2023-09-19 12:25:59 +0300

    Добавьте файлы проекта.

---
+++ Lab1Wk/Lab1Wk.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+
+int main()
+{
+	const int x{ 11 * 10 ^ -2 }, y{ 46 };
+	const float z{ 3.5 };
+
+	const float result{ (12 * x - x / y) + ((0.011 * y + z * 1.0 / x - 12) / (11)) };
+	std::cout << "Result = " << result;
+}
+
